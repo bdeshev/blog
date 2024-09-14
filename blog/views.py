c432b2b3c016c712719cac17ac811a4c54a479e0
bdeshev <grievous@abv.bg>
2024-09-14 11:54:11 +0300

    added tests

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,13 @@
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
+
 from .models import Post
 
 
 class BlogListView(ListView):
     model = Post
     template_name = "blog/home.html"
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = "blog/post_detail.html"
